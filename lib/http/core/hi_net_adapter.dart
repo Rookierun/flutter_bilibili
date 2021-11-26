9477831fb153845d68a0074159359ae2c79fc2cb
cuixiaoming <cuixm@133.cn>
2021-11-26 16:08:00 +0800

    基类简单封装

---
+++ lib/http/core/hi_net_adapter.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:flutter_bilibili/http/request/base_request.dart';
+
+abstract class HiNetAdapter {
+  Future<HiNetResponse<T>> send<T>(BaseRequest request);
+}
+
+/// 统一网络层返回格式
+class HiNetResponse<T> {
+  T data;
+  BaseRequest request;
+  int statusCode;
+  String statusMessage;
+  dynamic extra;
+
+  HiNetResponse(this.data, this.request, this.statusCode, this.statusMessage,
+      {this.extra});
+
+  @override
+  String toString() {
+    if (data is Map) {
+      return json.encode(data);
+    }
+    return data.toString();
+  }
+}

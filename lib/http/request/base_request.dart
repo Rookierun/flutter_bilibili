60aea4f090c9f2acfc305dbb057bcd5daaa7bc6a
cuixiaoming <cuixm@133.cn>
2021-11-26 09:59:49 +0800

    test request dart的单例模式的懒汉模式，一直提示变量未初始化

---
+++ lib/http/request/base_request.dart
@@ -0,0 +1,52 @@
+enum HttpMethod { GET, POST, DELETE }
+
+abstract class BaseRequest {
+  //支持查询参数，也应该支持path参数
+  var pathParams;
+  var useHttps = true;
+
+  //服务器域名
+  String authority() {
+    return "api.devio.org";
+  }
+
+  HttpMethod httpMethod();
+
+  String path();
+
+  String url() {
+    Uri uri;
+    var pathStr = path();
+    if (pathParams != null) {
+      if (path().contains("/")) {
+        pathStr = "${path()}/$pathParams";
+      } else {
+        pathStr = "${path()}$pathParams";
+      }
+    }
+    if (useHttps) {
+      uri = Uri.https(authority(), pathStr, params);
+    } else {
+      uri = Uri.http(authority(), pathStr, params);
+    }
+    print('uri:${uri.toString()}');
+    return uri.toString();
+  }
+
+  bool needLogin();
+
+  Map<String, String> params = {};
+
+  //添加参数
+  BaseRequest add(String key, Object value) {
+    params[key] = value.toString();
+    return this;
+  }
+
+  //header相关
+  Map<String, dynamic> header = {};
+  BaseRequest addHeader(String key, Object value) {
+    header[key] = value.toString();
+    return this;
+  }
+}
